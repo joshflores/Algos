137db89dc2a474df66177558471899f534292a68
Joshua Flores <49050743+joshflores@users.noreply.github.com>
2023-04-04 18:08:56 -0700

    Create countVowels.js

---
+++ 2023/countVowels.js
@@ -0,0 +1,13 @@
+function countVowels(str) {
+    let count = 0;
+    let vowels = "aeiouAEIOU";
+
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.indexOf(str[i]) !== -1) {
+            count++
+        }
+    }
+    return count;
+}
+
+console.log(countVowels('hello world'))
